783f3364236b48c4c716bbe6b837e575413925a0
Kyle <kyle@Kyles-MacBook-Pro.local>
2019-03-13 20:32:07 -0600

    Updates

---
+++ precache-manifest.39684f7bcb34e348507f2ecfb135e08d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d82890752e8ba0a87bfd4a85dc0cf0f",
+    "url": "/portfolio/static/media/gif-search.6d828907.png"
+  },
+  {
+    "revision": "a4f57d9fbb94a875923c",
+    "url": "/portfolio/static/css/main.35fa64ee.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "151e9ad79d072414b2b8",
+    "url": "/portfolio/static/js/2.11f34878.chunk.js"
+  },
+  {
+    "revision": "9152be177af58cd0aa28a6e0b33b7948",
+    "url": "/portfolio/static/media/man.9152be17.png"
+  },
+  {
+    "revision": "9d571721d35ff6776a51105906b8b943",
+    "url": "/portfolio/static/media/ghibli.9d571721.png"
+  },
+  {
+    "revision": "a4f57d9fbb94a875923c",
+    "url": "/portfolio/static/js/main.b2c00024.chunk.js"
+  },
+  {
+    "revision": "af360af9a291a10c33ffcb9356074f8a",
+    "url": "/portfolio/static/media/todo-table.af360af9.png"
+  },
+  {
+    "revision": "ff1c451df29935e0c8ad572c4a2be042",
+    "url": "/portfolio/static/media/modal.ff1c451d.png"
+  },
+  {
+    "revision": "75f5bb992aedbd8abb0e02a302f829dc",
+    "url": "/portfolio/static/media/album.75f5bb99.png"
+  },
+  {
+    "revision": "59f6d3e9fd58705d3e95f2bf974f6930",
+    "url": "/portfolio/static/media/pricing.59f6d3e9.png"
+  },
+  {
+    "revision": "357f9162b707b027a2ba2955709b8cb5",
+    "url": "/portfolio/index.html"
+  }
+];
